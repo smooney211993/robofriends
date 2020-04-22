3e39e910f421b1a432bbea3dac768ff6e6a75936
smooney211993 <sm.athome21@gmail.com>
2020-04-22 16:24:38 +1000

    import robot

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Card from './Components/Card/Card'
+import {robots} from './Components/Card/robots'
 
 class App extends React.Component {
   render() {
