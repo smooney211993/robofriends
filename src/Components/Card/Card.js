0d91a11dcec32ce1f6be0fc86a203275338db537
smooney211993 <sm.athome21@gmail.com>
2020-04-22 16:07:32 +1000

    resolved spelling mistake

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+const Card =()=> {
+    return (
+        <div>
+            <img alt="robots" src='https://robohash.org/yellow' />
+        </div>
+    )
+}
+
+
+
+export default Card;
