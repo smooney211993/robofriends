4b1c852b5fc3f67fbe49b7f9c1f50595d4b29c32
smooney211993 <sm.athome21@gmail.com>
2020-04-22 16:11:25 +1000

    added aother section

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,10 +4,14 @@ const Card =()=> {
     return (
         <div>
             <img alt="robots" src='https://robohash.org/yellow' />
+            <div>
+                <h2>Stephen Mooney</h2>
+                <p>gmail.com</p>
+            </div>
         </div>
     )
 }
 
 
 
-export default Card;
+export default Card;    
