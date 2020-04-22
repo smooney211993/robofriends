4cd08f0516bb7de7bc5e1d3557897d8c00df13d6
smooney211993 <sm.athome21@gmail.com>
2020-04-22 16:29:27 +1000

    added props

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 
-const Card =()=> {
+const Card =(props)=> {
     return (
         <div className ="bg-light-green dib br3 ma2 grow">
             <img alt="robots" src='https://robohash.org/yellow/test200x200' />
             <div>
-                <h2>Stephen Mooney</h2>
-                <p>gmail.com</p>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
             </div>
         </div>
     )
