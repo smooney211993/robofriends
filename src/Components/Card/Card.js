ff5810eb53341f08e492fa732611162dd8726c49
smooney211993 <sm.athome21@gmail.com>
2020-04-22 16:31:21 +1000

    added props id in the image source

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Card =(props)=> {
     return (
         <div className ="bg-light-green dib br3 ma2 grow">
-            <img alt="robots" src='https://robohash.org/yellow/test200x200' />
+            <img alt="robots" src={`https://robohash.org/${props.id}/test200x200`} />
             <div>
                 <h2>{props.name}</h2>
                 <p>{props.email}</p>
