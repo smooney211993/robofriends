0cb95afc967b6a37e7459b6b637bba000772891f
smooney211993 <sm.athome21@gmail.com>
2020-04-22 23:11:34 +1000

    revert back to orginal

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
-
-const Card =(props)=> {
+const Card = (props) =>{ 
     return (
         <div className ="bg-light-green dib br3 ma2 grow">
             <img alt="robots" src={`https://robohash.org/${props.id}/test200x200`} />
-            <div>
+             <div>
                 <h2>{props.name}</h2>
                 <p>{props.email}</p>
             </div>
         </div>
-    )
+     )
 }
 
 
