72c8c6ab83d6d0bfe53c99ec6eb96468a092139e
smooney211993 <sm.athome21@gmail.com>
2020-04-22 16:08:16 +1000

    imported Card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import card from './Components/Card/Card'
+import Card from './Components/Card/Card'
 
 class App extends React.Component {
   render() {
