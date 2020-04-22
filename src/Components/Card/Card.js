96db6abc96d6d043f5bcaece64ad1609d411cc89
smooney211993 <sm.athome21@gmail.com>
2020-04-22 16:13:30 +1000

    changed image size

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card =()=> {
     return (
         <div>
-            <img alt="robots" src='https://robohash.org/yellow' />
+            <img alt="robots" src='https://robohash.org/yellow/test200x200' />
             <div>
                 <h2>Stephen Mooney</h2>
                 <p>gmail.com</p>
