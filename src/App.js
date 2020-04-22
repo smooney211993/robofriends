bd20ac7a4fc2529efdd95ae21e2742da7a8ec8a2
smooney211993 <sm.athome21@gmail.com>
2020-04-22 17:29:01 +1000

    added cardlist

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Card from './Components/Card/Card'
 import {robots} from './Components/Card/robots'
+import CardList from './Components/Card/CardList'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-        
+        <CardList />
       </div>
     );
   }
