d938e4e9daa6c364aa76a0d31fbe20958c350678
smooney211993 <sm.athome21@gmail.com>
2020-04-26 14:56:39 +1000

    added robots as props and created functionality in search

--- src/Components/SearchBox/SearchBox.js
+++ src/Components/SearchBox/SearchBox.js
@@ -2,10 +2,37 @@ import React from 'react';
 
 
 class SearchBox extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            robots : this.props.robots,
+            searchfield: ''
+
+        }
+       
+    }
+
+    onSearchChange (event){
+            this.setState({searchfield: event.target.value})
+            const filteredRobots = this.state.robots.filter((robot)=>{
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+        console.log(filteredRobots);
+    }
+
+
     render() {
         return (
-            <div>
-                <input type="search" placeholder="Search Robots" />
+            <div className = "pa2">
+                <input 
+                type="search" 
+                placeholder="Search Robots"
+                onChange = {
+                    (event)=>{
+                        this.onSearchChange(event)
+                    }
+                }
+                 />
                 <button>Search</button>
             </div>
         );
