f03adc1cad0090a390f9c8368a2c2908414e00f0
smooney211993 <sm.athome21@gmail.com>
2020-05-03 15:11:29 +1000

    changed the class to a funciton and added error handler

--- src/Components/Card/CardList.js
+++ src/Components/Card/CardList.js
@@ -2,17 +2,16 @@ import React from 'react';
 import Card from './Card'
 
 
-class CardList extends React.Component {
-  render(){
-    return (
-      <div>
-         {this.props.robots.map((user, i)=>{
-          return <Card key={i} id ={this.props.robots[i].id} name={this.props.robots[i].name} email={this.props.robots[i].email} />
-      })}
-      </div>
+const CardList = ({robots}) =>{
+  
+  return (
+    <div>
+       {robots.map((user, i)=>{
+        return <Card key={i} id ={robots[i].id} name={robots[i].name} email={robots[i].email} />
+    })}
+    </div>
 
-    )
-  }
+  )
 }
 
 
